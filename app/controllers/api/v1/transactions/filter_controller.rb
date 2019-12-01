1f819300cb538740c1fe44ee807f3dc917aca9f7
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-01 12:48:36 -0700

    Add Transaction controller, serializer, and filter

---
+++ app/controllers/api/v1/transactions/filter_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::Transactions::FilterController < ApplicationController
+  def find
+    render json: TransactionSerializer.new(Transaction.find_by(find_params))
+  end
+
+  def find_all
+    render json: TransactionSerializer.new(Transaction.where(find_params).order(:id))
+  end
+
+  private
+    def find_params
+      params.permit(:id, :invoice_id, :credit_card_number, :result, :created_at, :updated_at)
+    end
+end
