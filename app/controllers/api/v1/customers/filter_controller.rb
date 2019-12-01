b138819ec5c4ca8733494703e994576d77a30c51
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-01 12:46:54 -0700

    Add Customer controller, serializer, and filter

---
+++ app/controllers/api/v1/customers/filter_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::Customers::FilterController < ApplicationController
+  def find
+    render json: CustomerSerializer.new(Customer.find_by(find_params))
+  end
+
+  def find_all
+    render json: CustomerSerializer.new(Customer.where(find_params))
+  end
+
+  private
+    def find_params
+      params.permit(:first_name, :last_name, :created_at, :updated_at, :id)
+    end
+end
