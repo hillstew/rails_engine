d52a286132450d3e38ef618818ae965f86f9cdad
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-01 12:48:07 -0700

    Add Items controller, serializer, and filter

---
+++ app/controllers/api/v1/items/filter_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::Items::FilterController < ApplicationController
+  def find
+    render json: ItemSerializer.new(Item.find_by(find_params))
+  end
+
+  def find_all
+    render json: ItemSerializer.new(Item.where(find_params).order(:id))
+  end
+
+  private
+    def find_params
+      params.permit(:id, :name, :description, :merchant_id, :unit_price, :created_at, :updated_at)
+    end
+end
