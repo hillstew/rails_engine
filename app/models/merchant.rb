2a63fe2903cb852d5a6f1af5388086efecc51ad9
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-02 08:41:35 -0700

    Add highest rev to Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,14 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def self.highest_revenue(quantity)
+    joins(:invoice_items, :transactions)
+      .select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+      .group(:id)
+      .order("revenue desc")
+      .limit(quantity)
+  end
 end
