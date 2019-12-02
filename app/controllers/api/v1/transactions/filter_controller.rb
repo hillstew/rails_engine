11b71084a8bf858a7d1ceb13aeac8359fcf1ebbb
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-01 17:02:11 -0700

    Add transactions random endpoint

--- app/controllers/api/v1/transactions/filter_controller.rb
+++ app/controllers/api/v1/transactions/filter_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::Transactions::FilterController < ApplicationController
     render json: TransactionSerializer.new(Transaction.where(find_params).order(:id))
   end
 
+  def random
+    render json: Transaction.random
+  end
+
   private
     def find_params
       params.permit(:id, :invoice_id, :credit_card_number, :result, :created_at, :updated_at)
