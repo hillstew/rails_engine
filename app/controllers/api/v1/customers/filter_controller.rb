9b6dd18599c63d29b226e4cd1dccd52a65e7735a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-12-01 17:00:48 -0700

    Add customers random endpoint

--- app/controllers/api/v1/customers/filter_controller.rb
+++ app/controllers/api/v1/customers/filter_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::Customers::FilterController < ApplicationController
     render json: CustomerSerializer.new(Customer.where(find_params))
   end
 
+  def random
+    render json: Customer.random
+  end
+
   private
     def find_params
       params.permit(:first_name, :last_name, :created_at, :updated_at, :id)
